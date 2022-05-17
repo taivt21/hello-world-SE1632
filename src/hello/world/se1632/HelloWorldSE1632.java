5ce45c739caa81b7727ee6d8a2682a992c8dc1ab
taivt218 <taivutan218@gmail.com>
2022-05-17 15:24:19 +0700

    1st time to play with git

---
+++ src/hello/world/se1632/HelloWorldSE1632.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.se1632;
+
+/**
+ *
+ * @author User
+ */
+public class HelloWorldSE1632 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello java and GitHub");
+    }
+    
+}
